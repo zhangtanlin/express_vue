8b7262f1a535cb1fc78464008acd5df82e622d4a
zhangtanlin90@126.com <zhangtanlin90@126.com>
2018-05-10 16:07:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'public'));
+
+//设置视图引擎
+app.engine('.html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
